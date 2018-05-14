ec4eaf47b8b3b5fd2eb9cda04438ed595716e895
Pavel <kpavelwork5@gmail.com>
2018-05-14 12:15:22 -0400

    v 0.0.1

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # InstafeedJS-Wrapper
+
+An easy way to work with [InstafeedJS](http://instafeedjs.com/) from HTML.
--- instafeed.wrapper.js
+++ instafeed.wrapper.js
@@ -1,26 +1,38 @@
-var InstafeedWrapper = (function(){
+/**
+ *  @fileOverview Describes the InstafeedWrapper module. An easy to work with InstafeedJS from HTML.
+ *
+ *  @author       Paul K.
+ *  @version      0.0.1
+ *
+ *  @requires     Instafeed:@link{http://instafeedjs.com/}
+ */
+
+var InstafeedWrapper = (function(Instafeed) {
     'use strict';
 
     /**
-     *
-     * @param
-     * @returns
+     * Local cache of initialized instagram feeds on the page.
+     * @type {Object}
      */
     var _instances = {};
 
     /**
-     *
-     * @param
-     * @returns
+     * Presetted OAuth parameters of the users.
+     * @type {Object}
      */
     var _userSecureOptions = {};
 
     /**
      * The InstafeedWrapper constructor.
      *
-     * @param {HTMLElement} container
-     * @param {Object} options
+     * @class
      * @constructor
+     *
+     * @property {HTMLElement} container
+     * @property {Number} ID
+     * @property {Object} options
+     * @property {Instafeed} instance
+     * @property {HTMLElement} loadMoreControl
      */
     function InstafeedWrapper(container, options) {
         this.container = container;
@@ -28,17 +40,23 @@ var InstafeedWrapper = (function(){
 
         this.instance = _instances[this.ID] = new Instafeed(this._overrideOptions(options));
         this.instance.run();
+
+        _addClass(this.container, 'ifw-container-loading');
+
         this._initLoadMoreControl();
     };
 
     /**
+     * Overrides common options by dataset of the container.
      *
      * @param {Object} options
-     * @return {Object}
+     * @private
+     * @returns {Object}
      */
     InstafeedWrapper.prototype._overrideOptions = function (options) {
         var dataStringMap = this.container.dataset;
         options.target = this.ID;
+        options = this._admixCallbacks(options);
 
         for(var property in dataStringMap) {
             if(property == 'user') {
@@ -63,33 +81,79 @@ var InstafeedWrapper = (function(){
     };
 
     /**
+     * Adds some core functionality to the callbacks.
      *
-     * @param
-     * @returns
+     * @param {Object} options
+     * @private
+     * @returns {Object}
+     */
+    InstafeedWrapper.prototype._admixCallbacks = function (options) {
+        var self = this,
+            userAfterCallback = options.after;
+
+        options.after = function() {
+            if(!self._initialized) {
+                _removeClass(self.container, 'ifw-container-loading');
+                self._initialized = true;
+            }
+
+            if(self.loadMoreControl && self.loadMoreControl.dataset.ifwLoadingContent && self.loadMoreControl.dataset.ifwBaseText) {
+                self.loadMoreControl.textContent = self.loadMoreControl.dataset.ifwBaseText;
+            }
+
+            if(!this.hasNext() && self.loadMoreControl) {
+                self.loadMoreControl.setAttribute('disabled', 'disabled');
+                _addClass(self.loadMoreControl, 'ifw-disabled');
+
+                if(self.loadMoreControl.dataset.ifwDisabledContent) {
+                    self.loadMoreControl.textContent = self.loadMoreControl.dataset.ifwDisabledContent;
+                }
+            }
+
+            if(userAfterCallback) userAfterCallback.apply(this, Array.prototype.slice(arguments, 0));
+        };
+
+        return options;
+    };
+
+    /**
+     * Initializes a button for dynamically loading images.
+     *
+     * @private
+     * @returns {InstafeedWrapper}
      */
     InstafeedWrapper.prototype._initLoadMoreControl = function () {
         var loadMoreControlId = this.container.dataset.loadMoreControl,
-            loadMoreControl,
             self = this;
 
-        if(!loadMoreControlId) return;
+        if(!loadMoreControlId) return this;
 
-        loadMoreControl = document.getElementById(loadMoreControlId);
+        this.loadMoreControl = document.getElementById(loadMoreControlId);
 
-        if(!loadMoreControl) return;
+        if(!this.loadMoreControl) return this;
 
-        loadMoreControl.addEventListener('click', function(event){
+        this.loadMoreControl.dataset.ifwBaseText = this.loadMoreControl.textContent;
+
+        this.loadMoreControl.addEventListener('click', function(event){
             self.instance.next();
+
+            if(this.dataset.ifwLoadingContent) {
+                this.textContent = this.dataset.ifwLoadingContent;
+            }
 
             event.preventDefault();
         }, false);
+
+        return this;
     };
 
     return {
         /**
+         * Initialization of the InstafeedWrapper.
          *
          * @param {HTMLCollection} containers
          * @param {Object} options
+         * @public
          */
         init: function(containers, options) {
             options = options || {};
@@ -111,6 +175,7 @@ var InstafeedWrapper = (function(){
          * Sets up users OAuth secure data.
          *
          * @param {Object} users
+         * @public
          */
         setUsersSecureOptions: function(users) {
             for(var user in users) {
@@ -126,4 +191,30 @@ var InstafeedWrapper = (function(){
             }
         }
     };
-})();
+
+    /**
+     * Adds class to the element.
+     *
+     * @param {HTMLElement} element element to which the class will be added
+     * @param {String} className class which will be added to the element
+     * @returns {HTMLElement} element with added class
+     */
+    function _addClass(element, className) {
+        element.className = element.className + ' ' + className;
+
+        return element;
+    };
+
+    /**
+     * Removes class from the element.
+     *
+     * @param {HTMLElement} element element from which the class will be removed
+     * @param {String} className class which will be removed from the element
+     * @returns {HTMLElement} element without specified class
+     */
+    function _removeClass(element, className) {
+        element.className = element.className.replace(className, '');
+
+        return element;
+    };
+})(window.Instafeed);
